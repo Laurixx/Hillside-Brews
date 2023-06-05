d0ecfda5a4faf92a2aca9d832cc3542d7a888ea6
Laurentiu Robu <43887989+Laurixx@users.noreply.github.com>
2023-06-05 13:16:34 +0100

    final

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 import { productsArray } from "./products.js";
 
 const menu = document.getElementById('menu')
+const basketHtml = document.getElementById('basket')
+
 const basket = []
 document.addEventListener('click', (e) => {
-    if(e.target.dataset.itemId){
+    if(e.target.dataset.itemId && e.target.classList.contains('add-to-basket')){
         addToBasket(e.target.dataset.itemId)
     }
-    if(e.target.classList.contains('pay')){
-        createItemsInBasket()
+    if(e.target.classList.contains('pay') || e.target.classList.contains('fa-circle-xmark')){
+        basketHtml.classList.toggle('hidden')
+
+    }
+    if(e.target.dataset.itemId && e.target.classList.contains('remove-from-basket')){
+        removeItem(e.target.dataset.itemId)
+    }
+    if (e.target.id === 'pay'){
+        pay()
+        e.preventDefault()
     }
 })
+
 
 function createMenu (){
     let item  = ``
@@ -27,7 +38,7 @@ function createMenu (){
             </div>
                 <div class="div2">
                     <div class="price">${product.price}£</div>
-                    <button class="add-to-basket" data-item-id="${product.uuid}">+</button>
+                    <button class="add-to-basket btx" data-item-id="${product.uuid}">+</button>
                 </div>
         </div>
 
@@ -47,10 +58,10 @@ function addToBasket (productId){
 function createFooter(){
     let footerhtml = `
     <div class="total">
-        <span>Total:</span>
+        <span> <i class="fa-solid fa-cart-shopping"></i>Total:</span>
         <span>${CalcTotal()}£</span>
     </div>
-    <button class="pay">Check Out</button>
+    <button class="pay pays">Check Out</button>
     `
     return footerhtml
 }
@@ -76,27 +87,61 @@ function createItemsInBasket (){
             </div>
                 <div class="div2">
                     <div class="price">${product.price}£</div>
-                    <button class="remove-from-basket" data-item-id="${product.uuid}"></button>
+                    <button class="remove-from-basket bta" data-item-id="${product.uuid}">-</button>
                 </div>
             </div>
-        
-            <span class="total">£</span>
-
         `
     })
+    
     return item 
+}
+// payment method to the modal 
+
+//show the basket when check out button is pressed 
+
+function removeItem(itemId){
+   const index = basket.findIndex((item) => {
+       return item.uuid === itemId
+    })
+    if (index !== - 1){
+        basket.splice(index, 1)
+    }
+    render()
+}
+function pay (){
+const email = document.getElementById('email').value
+const cvv = document.getElementById('cvv').value
+const card = document.getElementById('card-number').value
+const name = document.getElementById('name').value
+if(email === '' || cvv ==='' || card ==='' || name === ''){
+    const error = 'One of the fields is emplty '
+    document.getElementById('err').textContent = error
+    return
+}else{
+    document.querySelector('div.confirmation').innerHTML(`<p>Your order its on it's way ${name}</p>`)
+}
+// make this that somehow whem you press pay , it checks if all the inputs have been completed , and then it acts also clears the total and hides the footer
 }
 function render(){
     menu.innerHTML = createMenu()
+
+    /// THIS RENDERS THE FOOTER TO THE HTML 
     const footer = document.querySelector('footer')
     footer.innerHTML = createFooter()
+    //THIS ADDS AND REMOVES THE CLASS SO IT HELPS WITH THE ANNIMATION 
+     if(basket.length === 0) {
+         footer.classList.add(`hidden`)
+     }
+     else{
+         footer.classList.remove('hidden')
+     }
+    //THIS RENDERS THE ITEMS INTO THE CHECK OUT MODAL 
+    document.querySelector('div.items').innerHTML = createItemsInBasket()
+    //AND THIS ADDS THE TOTAL 
+    document.getElementById('check-out-total').textContent = `${CalcTotal()}£`
 
-    if(basket.length === 0) {
-        footer.classList.add(`hidden`)
+    if(basket.length === 0){
+        basketHtml.classList.toggle('hidden')
     }
-    else{
-        footer.classList.remove('hidden')
-    }
-    
 }
 render()
